7770ca815d1c0dccae960f95d12645e7622e2c13
SivaKumarK1 <sivakumar.k@comakeit.com>
2022-04-26 12:10:03 +0530

    Small Changes

--- quizApp/main.go
+++ quizApp/main.go
@@ -9,6 +9,8 @@ import (
 	"time"
 )
 
+// need to change and break down these function into smaller functions
+
 func main() {
 	csvFile := flag.String("csv", "problem.csv", "a csv file in the format of 'question,answer'")
 	timeLimit := flag.Int("limit",30, "the time limit for the quiz in seconds")
